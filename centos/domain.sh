e98fa3efc2281d877d8089d4031bf1459edce67f
bhikesh4pay <147730194+bhikesh4pay@users.noreply.github.com>
2023-12-05 14:52:39 +0530

    Update domain.sh

--- centos/domain.sh
+++ centos/domain.sh
@@ -1,22 +1,24 @@
 #/bin/bash
 
+vars_path=/home/ubuntu/APP_BACKOFFICE/ubuntu/vars/vars.yml
+updatestmts=/home/ubuntu/APP_BACKOFFICE/ubuntu/files/updatestmts.sql
 echo "This is configuration script"
 echo "Enter the domain name to be configured here"
 read dname
 echo "Enter the Database IP Address"
 read dbip
-sed -i "s/floor.low/$dname/g" /home/centos/APP_BACKOFFICE/centos/vars/vars.yml
-sed -i "s/floor.low/$dname/g" /home/centos/APP_BACKOFFICE/centos/files/updatestmts.sql
-sed -i "s/3.99.219.101/$dbip/g" /home/centos/APP_BACKOFFICE/centos/vars/vars.yml
+sed -i "s/floor.low/$dname/g" $vars_path
+sed -i "s/floor.low/$dname/g" $updatestmts
+sed -i "s/3.99.219.101/$dbip/g" $vars_path
 echo "Changes Completed!"
 echo "------------------------------------------------------------------------------------------"
-cat /home/centos/APP_BACKOFFICE/centos/vars/vars.yml
+cat $vars_path
 echo "------------------------------------------------------------------------------------------"
-cat /home/centos/APP_BACKOFFICE/centos/files/updatestmts.sql
+cat $updatestmts
 echo "------------------------------------------------------------------------------------------"
 svckuunda=$(openssl rand -base64 14)
-sed -i "s/djdDNDHSCziXwaNa8L4=/$svckuunda/g" /home/centos/APP_BACKOFFICE/centos/vars/vars.yml
+sed -i "s/djdDNDHSCziXwaNa8L4=/$svckuunda/g" $vars_path
 echo "Password for svc_kuunda is:" $svckuunda
 partnerkey=$(openssl rand -base64 14)
-sed -i "s/RBXtu6RflQ5HFMgu15Q=/$partnerkey/g" /home/centos/APP_BACKOFFICE/centos/vars/vars.yml
+sed -i "s/RBXtu6RflQ5HFMgu15Q=/$partnerkey/g" $vars_path
 echo "Password for Partnerkey is:" $partnerkey
